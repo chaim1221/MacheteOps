c3e22a93c550f536dd6b14481c9dbfb116af08dc
Chaim Eliyah <chaimeliyah@gmail.com>
2017-06-16 20:59:38 -0700

    skeleton of database copy script

--- PowerShell/azureFunctions/databaseCopy.ps1
+++ PowerShell/azureFunctions/databaseCopy.ps1
@@ -2,10 +2,10 @@
 # initial checkin. for a full example see:
 # https://docs.microsoft.com/en-us/azure/sql-database/scripts/sql-database-copy-database-to-new-server-powershell
 
-New-AzureRmSqlDatabaseCopy -ResourceGroupName "myResourceGroup" `
-    -ServerName $sourceserver `
-    -DatabaseName "MySampleDatabase" `
-    -CopyResourceGroupName "myResourceGroup" `
-    -CopyServerName $targetserver `
-    -CopyDatabaseName "CopyOfMySampleDatabase"
+New-AzureRmSqlDatabaseCopy -ResourceGroupName "Default-SQL-WestUS" `
+    -ServerName "$serverName" `
+    -DatabaseName "adventureworks" `
+    -CopyResourceGroupName "Default-SQL-WestUS" `
+    -CopyServerName "$serverName" `
+    -CopyDatabaseName "adventureworks-test"
 
