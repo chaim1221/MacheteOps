09d120ac2cecdc6bf94f5a30a2ef7a679039c0d0
Chaim Eliyah <chaim.eliyah@cambiahealth.com>
2018-11-17 20:24:39 -0800

    retire this repo

---
+++ README.md
@@ -0,0 +1,2 @@
+This project has been moved under 
+[machete-dotnet-tools](https://github.com/chaim1221/machete-dotnet-tools).
--- SQL/prod/workOrderCompletionUpdateQuery.sql
+++
@@ -1,9 +0,0 @@
---select * from dbo.ReportDefinitions
---select * from lookups
-
-update workorders 
-set workorders.status = 44
-from workorders wo
-join workassignments wa on wa.workorderid = wo.id
-where status = 42
-and wa.workerassignedid is not null
